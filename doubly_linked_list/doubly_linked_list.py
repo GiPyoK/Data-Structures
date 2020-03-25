80d652802dd64414a5fdacb59b45589f702124c0
Gi Pyo Kim <gipyok@gmail.com>
2020-03-24 17:02:02 -0700

    dll

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -66,9 +66,12 @@ class DoublyLinkedList:
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
     def remove_from_head(self):
-        value = self.head.value
-        self.delete(self.head)
-        return value
+        if self.head == None:
+            return None
+        else:
+            value = self.head.value
+            self.delete(self.head)
+            return value
 
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
@@ -88,9 +91,12 @@ class DoublyLinkedList:
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
-        value = self.tail.value
-        self.delete(self.tail)
-        return value
+        if self.tail == None:
+            return None
+        else:
+            value = self.tail.value
+            self.delete(self.tail)
+            return value
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
@@ -118,10 +124,13 @@ class DoublyLinkedList:
             self.tail = None
         elif node == self.head:
             self.head = self.head.next
+            self.head.prev = None
         elif node == self.tail:
             self.tail = self.tail.prev
+            self.tail.next = None
+        else:
+            node.delete()
         
-        node.delete()
         self.length -= 1
         
     """Returns the highest value currently in the list"""
