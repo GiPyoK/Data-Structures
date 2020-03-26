6830856fe446f3ab1c8a015aa6e5795c90f9ca94
Gi Pyo Kim <gipyok@gmail.com>
2020-03-26 14:01:31 -0700

    BST pre_order_dft and post_order_dtf implemented

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -162,21 +162,33 @@ class BinarySearchTree:
 
     # Print Pre-order recursive DFT
     def pre_order_dft(self, node):
-        pass
+        if node:
+            print(node.value)
+        
+        if node.left:
+            self.pre_order_dft(node.left)
+        
+        if node.right:
+            self.pre_order_dft(node.right)
 
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
-        pass
+        if node.left:
+            self.post_order_dft(node.left)
+        
+        if node.right:
+            self.post_order_dft(node.right)
 
+        print(node.value)
 
-# test
-bst = BinarySearchTree(1)
-bst.insert(8)
-bst.insert(5)
-bst.insert(7)
-bst.insert(6)
-bst.insert(3)
-bst.insert(4)
-bst.insert(2)
+# # test
+# bst = BinarySearchTree(1)
+# bst.insert(8)
+# bst.insert(5)
+# bst.insert(7)
+# bst.insert(6)
+# bst.insert(3)
+# bst.insert(4)
+# bst.insert(2)
 
-bst.dft_print(bst)
+# bst.post_order_dft(bst)
